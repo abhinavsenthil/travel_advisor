7705c5f0213123e60e1d3f0a94fe1c1e109fe4d8
abhinavsenthil <abhinav.senthilc@gmail.com>
2023-06-14 22:36:25 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import Header from './Components/Header/Header';
+import List from './Components/List/List';
+import PlaceDetails from './Components/PlaceDetails/PlaceDetails';
+import Map from './Components/Map/Map';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { getPlacesData, getWeatherData } from './api';
+
+import { CssBaseline, Grid } from '@material-ui/core';
+
+
+const App = () => {
+
+    const [places, setPlaces] = useState([]);
+
+    const [coordinates, setCoordinates] = useState({});
+    const [bounds, setBounds] = useState({});
+    const [childClicked, setChildClicked] = useState(null);
+
+    const [isLoading, setIsLoading] = useState(false);
+
+    const [type, setType] = useState('restaurants');
+    const [rating, setRating] = useState('');
+
+    const [filteredPlaces, setFilteredPlaces] = useState([]);
+
+    const [weathetData, setWeatherData] = useState([])
+
+ 
+
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition(({ coords: {latitude, longitude}}) => {
+            setCoordinates({lat:latitude, lng:longitude});
+        })
+    }, []);
+
+    useEffect(() =>{
+        const filteredPlaces = places.filter((place) => place.rating > rating);
+        setFilteredPlaces(filteredPlaces);
+    }, [rating]);
+
+    useEffect(() => {
+        setIsLoading(true);
+
+        getWeatherData(coordinates.lat, coordinates.lng)
+        .then((data) => setWeatherData(data));
+
+        if (coordinates && bounds) {
+          getPlacesData(type, bounds.sw, bounds.ne)
+            .then((data) => {
+              console.log(data);
+              setPlaces(data.filter((place) => place.name && place.num_reviews > 0));
+              setFilteredPlaces([]);
+              setIsLoading(false);
+
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+        }
+    
+      }, [type, bounds]);
+      
+    
+
+    return (
+        <>
+            <CssBaseline />
+            <Header setCoordinates = {setCoordinates} />
+            <Grid container spacing={3} style={{ width: '100%' }}>
+                <Grid item xs={12} md={4}>
+                    <List 
+                        places={filteredPlaces.length ? filteredPlaces : places}
+                        childClicked = {childClicked}
+                        isLoading = {isLoading}
+                        type = {type}
+                        rating = {rating}
+                        setType = {setType}
+                        setRating = {setRating} // not a best practice, have to use redux if you need to pass down a component of the list
+                         />
+                </Grid>
+                <Grid item xs={12} md={8}>
+                    <Map 
+                        setCoordinates = {setCoordinates}
+                        setBounds = {setBounds}
+                        coordinates = {coordinates}
+                        places={filteredPlaces.length ? filteredPlaces : places}
+                        setChildClicked = {setChildClicked}
+                        weatherData = {weathetData}
+                        //only sent one level deep, if more, use redux
+                    />
+                </Grid>
+            </Grid>
+
+        </>
+    );
 }
 
 export default App;
